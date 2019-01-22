0566d08256a5275190c0f1b7a63cd82cb6c51918
Noah Kim <noahbkim@gmail.com>
2019-01-22 00:48:05 -0800

    Add indentation convenience function

--- tests/tests.py
+++ tests/tests.py
@@ -6,6 +6,10 @@ from grade.test import Target, Result, register
 from grade.test.middleware import iterated
 
 
+def i(size: int):
+    return " " * (size - 1)
+
+
 @register(middleware=iterated)
 def test_pass(target: Target):
     """Basic pass."""
@@ -23,7 +27,7 @@ def test_fail(target: Target):
     passing = runtime.stdout.strip() == "pass"
     yield Result(runtime, passing)
     if not passing:
-        print("  expected pass, got", runtime.stdout.strip())
+        print(i(2), "expected pass, got", runtime.stdout.strip())
 
 
 @register(middleware=iterated)
@@ -33,13 +37,13 @@ def test_error(target: Target):
     runtime = target.run("error", timeout=1.0)
     if runtime.code != 0:
         yield Result(runtime, False)
-        print("  received return code", runtime.code)
+        print(i(2), "received return code", runtime.code)
         for line in filter(None, runtime.stderr.split("\n")):
-            print("    " + line)
+            print(i(4), line)
 
     passing = runtime.stdout.strip() == "pass"
     yield Result(runtime, passing)
-    print("  expected pass, got fail")
+    print(i(2), "expected pass, got fail")
 
 
 @register(middleware=iterated)
@@ -49,15 +53,15 @@ def test_fault(target: Target):
     runtime = target.run("fault", timeout=1.0)
     if runtime.code != 0:
         yield Result(runtime, False)
-        print("  received return code", runtime.code)
+        print(i(2), "received return code", runtime.code)
         for line in filter(None, runtime.stderr.split("\n")):
-            print("    " + line)
+            print(i(4), line)
         if runtime.code == -11:
-            print("    segmentation fault")
+            print(i(4), "segmentation fault")
 
     passing = runtime.stdout.strip() == "pass"
     yield Result(runtime, passing)
-    print("  expected pass, got fail")
+    print(i(2), "expected pass, got fail")
 
 
 @register(middleware=iterated)
@@ -71,15 +75,15 @@ def test_timeout(target: Target):
 
     if runtime.code != 0:
         yield Result(runtime, False)
-        print("  received return code", runtime.code)
+        print(i(2), "received return code", runtime.code)
         for line in filter(None, runtime.stderr.split("\n")):
-            print("    " + line)
+            print(i(4), line)
         if runtime.code == -11:
-            print("    segmentation fault")
+            print(i(4), "segmentation fault")
 
     passing = runtime.stdout.strip() == "pass"
     yield Result(runtime, passing)
-    print("  expected pass, got fail")
+    print(i(2), "expected pass, got fail")
 
 
 if __name__ == "__main__":
