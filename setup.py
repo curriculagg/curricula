fcc2e917e06f6bce300bfee571542a109917d00d
Noah Kim <noahbkim@gmail.com>
2019-01-30 22:55:32 -0800

    Add setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="grade",
+      version="0.1",
+      description="A grading toolkit for evaluating student code.",
+      url="https://github.com/csci104/grade",
+      author="Noah Kim",
+      author_email="noahbkim@gmail.com",
+      packages=("grade",),
+      zip_safe=False)
