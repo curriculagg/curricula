40a3c7a70ad1b110cc4f35ec0bbb85c0cafc88ae
Noah Kim <noahbkim@gmail.com>
2020-01-05 19:47:54 -0500

    Clean up for distribution

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 from setuptools import setup, find_packages
 
-setup(name="curricula",
-      version="0.0.3",
-      description="A content manager and grading toolkit for evaluating student code",
-      url="https://github.com/csci104/curricula",
-      author="Noah Kim",
-      author_email="noahbkim@gmail.com",
-      packages=find_packages(),
-      zip_safe=False,
-      install_requires=["jinja2", "jsonschema"])
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="curricula",
+    version="0.0.3",
+    description="A content manager and grading toolkit for evaluating student code",
+    url="https://github.com/csci104/curricula",
+    author="Noah Kim",
+    author_email="noahbkim@gmail.com",
+
+    # Extra
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    # Python
+    python_requires=">=3.7",
+
+    # Packaging
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=["jinja2", "jsonschema"])
