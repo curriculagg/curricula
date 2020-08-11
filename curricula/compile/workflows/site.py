89d5d919ec2483d7dd8d607b59579bedf4d0fc5d
Noah Kim <noahbkim@gmail.com>
2020-08-11 11:05:40 -0400

    Bump version

--- curricula/compile/workflows/site.py
+++ curricula/compile/workflows/site.py
@@ -19,11 +19,12 @@ class SiteWorkflow(Workflow):
         if not result["instructions"].compiled:
             return
 
-        site_path = Path(self.configuration.options["site"]).joinpath(assignment.short)
-        if not site_path.exists():
-            site_path.mkdir(exist_ok=True, parents=True)
-        log.info(f"pipelining instructions to site {site_path}")
+        site_path = Path(self.configuration.options["site"])
+        if not site_path.parent.exists():
+            log.error("site parent path does not exist, cancelling pipeline")
+            return
 
+        log.info(f"pipelining instructions to site {site_path}")
         copy_directory(
             source=self.configuration.artifacts_path.joinpath(Paths.INSTRUCTIONS),
             destination=site_path,
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="curricula",
-    version="1.2.3",
+    version="1.2.4",
     description="A content manager and grading toolkit for evaluating student code",
     url="https://github.com/csci104/curricula",
     author="Noah Kim",
